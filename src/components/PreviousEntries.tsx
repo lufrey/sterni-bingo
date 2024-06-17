b71db993ea1d68a408047de3f672ea129943c17b
Lukas Frey <lukas.frey03@outlook.de>
2024-06-17 03:42:07 +0200

    add message for empty entries

--- src/components/PreviousEntries.tsx
+++ src/components/PreviousEntries.tsx
@@ -3,6 +3,14 @@ import { deleteEntry } from "@/app/actions";
 import { Entry } from "@/db/schema";
 
 export function PreviousEntries(props: { entries: Entry[]; username: string }) {
+    if (props.entries.length === 0) {
+        return (
+            <div className="flex font-mono justify-center pb-6">
+                Keine Einträge vorhanden
+            </div>
+        );
+    }
+
     return (
         <div className="flex gap-2 font-mono justify-center pb-6 flex-wrap">
             <span>Letze Einträge:</span>
