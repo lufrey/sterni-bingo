1984e523efcbecc4125ef2b04a3477c249f119eb
Lukas Frey <lukas.frey03@outlook.de>
2024-06-17 02:48:54 +0200

    use db url from env

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,9 +2,11 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
+const db_url = process.env.DB_URL;
+
 export const db = drizzle(
     createClient({
-        url: "file:src/db/sqlite.db",
+        url: `file:${db_url}`,
     }),
     {
         schema,
